e5e1330c847265c7bb371b2b5dbe71ce1b71eff9
rathu chandran <46434790+rathu26@users.noreply.github.com>
2019-05-28 18:58:13 +0700

    Update MOD1.py

--- MOD1.py
+++ MOD1.py
@@ -308,4 +308,15 @@ result=sum_of_list(num_list)
 print("Sum of the elements:",result)
 
 
+#PF-Tryout-46
 
+def doublePreceding (values):
+    if len(values) > 0:
+        previous = values[0]
+        values[0] = 0
+        for idx in range(1, len(values)):
+            values[idx] = 2 * previous
+            previous = values[idx]
+    return values
+    
+print(doublePreceding([3,8,2]))
