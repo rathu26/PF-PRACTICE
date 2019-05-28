07b0fe85b3b120b55c9bd6d4c9da07cbda9ef0a3
rathu chandran <46434790+rathu26@users.noreply.github.com>
2019-05-28 21:23:58 +0530

    Update MOD1.py

--- MOD1.py
+++ MOD1.py
@@ -205,6 +205,22 @@ def check_22(num_list):
 print(check_22([3,2,5,1,2,1,2,2]))
 
 
+#PF-Prac-16
+def rotate_list(input_list,n):
+    output_list=[]
+    for i in range(len(input_list)-n,len(input_list)):
+        output_list.append(input_list[i])
+    for i in range(0,len(input_list)-n):
+        output_list.append(input_list[i])
+    return output_list
+    
+    return output_list
+
+input_list= [1,2,3,4,5,6]
+output_list=rotate_list(input_list,4)
+print(output_list)
+
+
 #PF-Tryout 22
 def diagonal_stars(number):
    #start writing your code here
