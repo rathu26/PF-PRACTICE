f910481d008d41619178cdffd516de09777ebaab
rathu chandran <46434790+rathu26@users.noreply.github.com>
2019-05-28 21:30:48 +0530

    Update MOD1.py

--- MOD1.py
+++ MOD1.py
@@ -221,6 +221,23 @@ output_list=rotate_list(input_list,4)
 print(output_list)
 
 
+#PF-Prac-21
+def check_numbers(num1,num2):
+    #start writing your code here
+    num_list=[]
+    for i in range(num1,num2+1):
+        for j in range(num1,num2+1):
+            if(i%j==0 and i not in num_list and i!=j):
+                num_list.append(i)
+    count=len(num_list)
+    return [num_list,count]
+
+num1=2
+num2=20
+print(check_numbers(num1, num2))
+
+
+
 #PF-Tryout 22
 def diagonal_stars(number):
    #start writing your code here
